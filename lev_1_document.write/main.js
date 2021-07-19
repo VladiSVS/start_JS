91be26e5271c506aa78293a1889948a77a95291a
VladiSVS <uladzimir.siauko@gmail.com>
2021-07-19 15:50:01 +0200

    document.write

---
+++ lev_1_document.write/main.js
@@ -0,0 +1,6 @@
+let myText = "Hello again"
+let name = "John"
+let nachname = "Schmidt"
+document.write(myText + "</br>")
+document.write("Name: " + name + "</br>")
+document.write("Nachname: " + nachname + "</br>")
